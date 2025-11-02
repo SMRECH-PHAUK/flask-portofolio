7e51a5b3adb7213854dbc6978e84e929d964c47c
SAMRECH-PHAUK <samrechkh235e1>
2025-11-02 17:33:57 +0700

    Add deployment files for Heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from database import db
 from models import *
@@ -25,4 +26,4 @@ def project2():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
